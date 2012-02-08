6d562649aaf3c87971e9b0cd0644c73fd7699383
Mauricio Zuardi <mauricio.zuardi@gmail.com>
2012-02-08 12:14:48 -0200

    Atividades na tela + Selecionando-as (próximo passo, mudar o fundo)

--- js/InterfaceManager.js
+++ js/InterfaceManager.js
@@ -111,6 +111,7 @@ InterfaceManager.prototype.drawContents = function(){
 	this.drawTimeline();
 	
 	//desenha as atividades
+	this.drawActivities();
 	
 }
 
@@ -129,8 +130,217 @@ InterfaceManager.prototype.drawTimeline = function(){
 	this.updateScreen();
 }
 
-InterfaceManager.prototype.resizeTimeline = function(){
+InterfaceManager.prototype.drawActivities = function(){
+	var atividades = this.dataManager.atividades;
+	//cria um array com todas as atividades (para poder ordenar)
+	var sorted = [];
+	for(var s in atividades){
+		for(var a in atividades[s]){
+			var obj = atividades[s][a];
+			obj.context = this;
+			sorted.push(obj);
+		}
+	}
 	
+	//define como deve ordenar
+	if(this.dataManager.currentSite.ordem && !this.dataManager.query){
+		//se tem algo especificado no cadastro do site e não é um resultado de busca
+		switch(this.dataManager.currentSite.ordem.toLowerCase()){
+			case 'inversa':
+				sorted.sort(InterfaceManager.ordemDataInicialAscendente);
+			break;
+			case 'alfabetica':
+				sorted.sort(InterfaceManager.ordemAlfabeticaNome);
+			break;
+			case 'alfabética':
+				sorted.sort(InterfaceManager.ordemAlfabeticaNome);
+			break;
+			default:
+				console.log('ERRO: Ordem desconhecida. Usando ordem default.');
+			break;
+		}
+	} else if(this.dataManager.query){
+		//em páginas de busca, usar ordem inversa
+		sorted.sort(InterfaceManager.ordemDataInicialAscendente);
+	} else {
+		//senão, default (data inicial)
+		sorted.sort(InterfaceManager.ordemDataInicialDescendente);
+	}
+		
+	//percorre o array criando o HTML
+	for(var i in sorted){
+		var a = sorted[i];
+		a.idComposto = a.idSiteOriginal + '-' + a.id;
+		var id = a.idComposto;
+		var labelTxt = a.nome;
+		var past = (a.datafinal.getTime() < Date.now() && this.dataManager.currentSite.passadorelevante == '0') ? " past" : "";
+		
+		//cria o DIV com id com a bolinha, range e label dentro
+		var html = "<div data-id='" + id + "' class='event " + id + past +"'><span data-id='" + id + "' class='range'><span data-id='" + id + "' class='dot" + past + "'></span></span><span class='label'>" + labelTxt + "<img src='./img/interface/nano-balloon.gif' class='nano' /></span></div>";
+		$('.activities').append(html);
+		
+		//aplica as classes baseado no status
+		var p = $.proxy(InterfaceManager.updateHTMLClass, a); p(this.dataManager.timeline.timeMarks);
+		
+		//aplica os cliques
+		var id = a.idSiteOriginal + '-' + a.id;
+		var range = $('div.' + id + ' .range');
+		var label = $('div.' + id + ' .label');
+		range.click($.proxy(InterfaceManager.dotOrRangeClicked, a));
+		label.click($.proxy(InterfaceManager.labelClicked, a));
+	}
+	
+	//aplica o mouse over nos labels
+	$('.label').mouseover(function(event){
+		$(event.target).addClass('over');
+	});
+	
+	$('.label').mouseout(function(event){
+		$(event.target).removeClass('over');
+	});
+}
+
+InterfaceManager.dotOrRangeClicked = function(event){
+	//define o elemento do HTML
+	var e = $(event.target);
+	var id = this.idComposto;
+	
+	//altera o apontamento para dot se tiver clicado em range
+	if(e.hasClass('range')){
+		e = $('div.' + id + ' .dot');
+		InterfaceManager.rangeClicked(e, this)
+	} else {
+		InterfaceManager.dotClicked(e, this);
+	}
+}
+
+InterfaceManager.dotClicked = function(e, atividade){
+	InterfaceManager.selectActivity(atividade);
+	// mostraInfo();
+}
+
+InterfaceManager.rangeClicked = function(e, atividade){
+	InterfaceManager.selectActivity(atividade);
+	// mostraInfo();
+}
+
+InterfaceManager.selectActivity = function(atividade){
+	var atividades = atividade.context.dataManager.atividades;
+	//percorre todas as atividades procurando a atividade que veio como parâmetro
+	for(var s in atividades){
+		for(var a in atividades[s]){
+			var obj = atividades[s][a];
+			//se for a atividade que procuramos
+			if(obj == atividade){
+				//se tem só uma letra - ou seja, não estava previamente selecionado, selecione
+				if(obj.visual.length == 1){
+					obj.visual += 's';
+					//atualiza o dataManager
+					atividade.context.dataManager.atividadeSelecionada = atividade;
+				}
+			} else {	
+				//tem mais que uma letra - estava selecionado - pega só a primeira (tira a seleção)
+				if(obj.visual.length > 1){
+					obj.visual = obj.visual.substr(0,1);
+				}
+			}
+			//atualiza o visual
+			var p = $.proxy(InterfaceManager.updateHTMLClass, obj);
+			p(obj.context.dataManager.timeline.timeMarks);
+		}
+	}
+}
+
+InterfaceManager.updateHTMLClass = function(timeline){
+	var id = this.idComposto;
+	var div = $('div.' + id);
+	var dot = $('div.' + id + ' .dot');
+	var range = $('div.' + id + ' .range');
+	var label = $('div.' + id + ' .label');
+	// var ml = 0;
+	
+	switch (this.visual){
+		//pequeno
+		case "p":
+			if(dot.hasClass('selected'))		dot.removeClass('selected');
+			if(dot.hasClass('big'))					dot.removeClass('big');
+			if(!range.hasClass('mini'))			range.addClass('mini');
+			if(!label.hasClass('mini'))			label.addClass('mini');
+		break;
+		
+		//pequeno e selecionado
+		case "ps":
+			if(!dot.hasClass('selected'))		dot.addClass('selected');
+			if(dot.hasClass('big'))					dot.removeClass('big');
+			if(!range.hasClass('mini'))			range.addClass('mini');
+			if(!label.hasClass('mini'))			label.addClass('mini');
+			// mudaFundo(this.id);
+		break;
+		
+		//grande
+		case "g":
+			if(dot.hasClass('selected'))		dot.removeClass('selected');
+			if(!dot.hasClass('big'))				dot.addClass('big');
+			if(range.hasClass('mini'))			range.removeClass('mini');
+			if(label.hasClass('mini'))			label.removeClass('mini');
+		break;
+		
+		//grande e selecionado
+		case "gs":
+			if(!dot.hasClass('selected'))		dot.addClass('selected');
+			if(!dot.hasClass('big'))				dot.addClass('big');
+			if(range.hasClass('mini'))			range.removeClass('mini');
+			if(label.hasClass('mini'))			label.removeClass('mini');
+			// mudaFundo(this.id);
+		break;
+	}
+	
+	//posiciona
+	var t = Date.now();
+	var t0 = this.datainicial.getTime();
+	var t1 = this.datafinal.getTime();
+	
+	var COMP_DOT_BIG = 5 - dot.outerWidth(false)/2;
+	var COMP_DOT = 1;
+	var FINE_TUNING = dot.hasClass('big') ? COMP_DOT_BIG : COMP_DOT;
+	
+	// //posiciona o começo do evento
+	var x0 = InterfaceManager.timeToPosition(t0, timeline) + FINE_TUNING;
+	div.css('margin-left', x0);
+	
+	//posiciona o fim do evento
+	var x1 = InterfaceManager.timeToPosition(t1, timeline) + FINE_TUNING;
+	
+	var rangeEnd = x1-x0;
+	var length = rangeEnd + dot.outerWidth(false);	//compensa o tamanho da bolinha
+	range.css('width', length);
+	
+	//posiciona a bolinha, representando o progresso geral do evento
+	t = (t < t0) ? t0 : t;
+	t = (t > t1) ? t1 : t;
+	var x = InterfaceManager.timeToPosition(t, timeline) + FINE_TUNING - x0;
+	dot.css('margin-left', x);
+	
+	//posiciona o label
+	var ml = parseInt(dot.css('margin-left')) + dot.outerWidth(false) + 7;
+	//aplica
+	label.css('margin-left', ml);
+}
+
+InterfaceManager.ordemDataInicialDescendente = function(a,b){
+	return b.datainicial - a.datainicial;
+}
+
+InterfaceManager.ordemDataInicialAscendente = function(a,b){
+	return a.datainicial - b.datainicial;
+}
+
+InterfaceManager.ordemAlfabeticaNome = function(a,b){
+	var sA = DataManager.stringToSlug(a.nome);
+	var sB = DataManager.stringToSlug(b.nome);
+	if (sA < sB) {return -1}
+	if (sA > sB) {return 1}
+	return 0;
 }
 
 InterfaceManager.prototype.defineDestaques = function(){
