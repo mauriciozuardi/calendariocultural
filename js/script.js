49a5418764b8cf41390571e17f060e3a0c722c4b
Mauricio Zuardi <mauricio.zuardi@gmail.com>
2012-02-24 11:09:12 -0200

    Chrome Frame + alerta para Firefoxes velhos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-function init(){
-	// Chrome Frame check.
+function init(){	
+	// Chrome Frame check. Mostra para todos os IEs.
   CFInstall.check({
     mode: "overlay",
     preventInstallDetection: 'true'
   });
+
+	//checa requisitos
+	var version = parseInt(navigator.appVersion);
+	if(!$.support.ajax || (navigator.appName == 'Netscape' && version < 5)){
+		var shouldRedirect = confirm('Desculpe, mas para acessar nosso site é recomendado um navegador mais atual.\nGostaria de instalar o Google Chrome?');
+		shouldRedirect ? window.location = 'https://www.google.com/chrome/' : history.go(-1);
+	} else {
+		console.log('Navegador ok. Versão:' + version);
+	}
 
 	//init
 	im = new InterfaceManager();
